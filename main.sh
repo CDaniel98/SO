813b3976e2d37161aace9e607c1b8a6617a5b972
CDaniel98 <estevescarlitos@gmail.com>
2019-03-11 19:11:46 +0000

    sh files

---
+++ main.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Bash Menu Script Example
+
+echo "Menu Principal"
+echo " "
+
+PS3='Introduza a opção: '
+options=("Registo/Editar" "ALterar Dados" "Visualizar Dados" "Relatórios" "Sair do Programa")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Registo/Editar")
+		bash "registo.sh"
+		    ;;
+        "ALterar Dados")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Visualizar Dados")
+            echo "you chose choice $REPLY which is $opt"
+			
+            ;;
+		"Relatórios")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Sair do Programa")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
---
+++ registo.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Bash Menu Script Example
+
+			echo ""
+            echo "Menu Registo/Editar"
+			echo " "
+			PS3='Introduza a opção: '
+			options_registo=("Universidade" "Professor" "Estudante" "Disciplina" "Menu anterior")
+			select opt_r in "${options_registo[@]}"
+			do
+				case $opt_r in
+					"Universidade")
+						echo "Registo de $opt_r"
+						;;
+					"Professor")
+						echo "Registo de $opt_r"
+						;;
+					"Discplina")
+						echo "Registo de $opt_r"
+						;;
+					"Menu anterior")
+						bash "main.sh"
+						;;
+					*) echo "invalid option $REPLY";;
+				esac
+			done
+			
+			
+			
+            
