04d930c803d2a4d8d5d7384e5971a8ec4770bed7
antoniojmabreu <antoniojmabreu@gmail.com>
2019-04-07 09:43:48 +0100

    Apagar e alterar profs e unis a funcionar

--- alterar_dados.sh
+++ alterar_dados.sh
@@ -2,7 +2,7 @@
 # Bash Menu Script Example
 
 echo ""
-echo "Menu Alterar dados"
+echo "Menu Alterar/Apagar dados"
 echo " "
 f_alunos="alunos.txt"
 f_profs="profs.txt"
@@ -10,89 +10,188 @@ f_disc="disc.txt"
 f_uni="uni.txt"
 f_dados="dados.txt"
 
+
+
 PS3='Introduza a opção:'
-options_registo=("Universidade" "Professor" "Disciplina" "Estudante"  "Menu anterior")
-
-select opt_r in "${options_registo[@]}"
+options_registo_inicial=("Apagar" "Alterar"  "Menu anterior")
+select opt_ri in "${options_registo_inicial[@]}"
 do
-	case $opt_r in
-		"Universidade")
-			echo "Aletrar $opt_r"
-			echo " "
-			echo "Insira o nome da Universidade a alterar:"
-      cat $f_uni
-			read r_uni
+	case $opt_ri in
+		"Apagar")
 
-			if grep -Fxq $r_uni $f_uni
-				then
-	        echo "Insira o novo nome"
-	        read new
-	        sed -i "s/$r_uni/$new/" $f_uni
-					echo $r_uni"->"$new
-					#Chamada do ficheiro novamente para aparecerem as opções do menu
-					bash "registo.sh"
-	        echo " "
-					break;
+			PS3='Introduza a opção:'
+			options_registo=("Universidade" "Professor" "Disciplina" "Estudante"  "Menu anterior")
 
-				else
-	        echo "ERRO: Esta Universidade não se encontra registada! "
-			fi;;
+			select opt_r in "${options_registo[@]}"
+			do
+				case $opt_r in
+					"Universidade")
+						echo "Aletrar $opt_r"
+						echo " "
+						echo "Insira o nome da Universidade a alterar:"
+						cat $f_uni
+						read r_uni
 
-		 "Professor")
-				echo "Aletrar $opt_r"
-				echo " "
-				echo "Insira o nome do Professor a alterar:"
-	      cat $f_profs
-				read r_prof
+						if grep -Fxq $r_uni $f_uni
+							then
+								sed -i -e "s/$r_uni//g" $f_uni
+								sed -i -e "s/$r_uni//g" $f_dados
+								#Chamada do ficheiro novamente para aparecerem as opções do menu
+								bash "registo.sh"
+								echo " "
+								break;
 
-				if grep -Fxq $r_prof $f_profs
-					then
-            echo "Insira o novo nome"
-            read new
-            sed -i "s/$r_prof/$new/" $f_profs
-						echo $r_prof"->"$new
-		                #Se o professor estiver designado a algum aluno altera-se?
-						#Chamada do ficheiro novamente para aparecerem as opções do menu
-						bash "registo.sh"
-		        echo " "
-						break;
+							else
+								echo "ERRO: Esta Universidade não se encontra registada! "
+						fi;;
 
-					else
-		      	echo "ERRO: Este docente não se encontra registado! "
-				fi;;
+					 "Professor")
+							echo "Aletrar $opt_r"
+							echo " "
+							echo "Insira o nome do Professor a alterar:"
+							cat $f_profs
+							read r_prof
 
-		"Disciplina")
-			echo "Aletrar $opt_r"
-			echo " "
-			echo "Insira o nome da Disciplina a alterar:"
-      cat $f_disc
-			read r_disc
+							if grep -Fxq $r_prof $f_profs
+								then
+									sed -i -e "s/$r_prof//g" $f_profs
+									sed -i -e "s/$r_prof//g" $f_dados
+									bash "registo.sh"
+									echo " "
+									break;
 
-			if grep -Fxq $r_disc $f_disc
-				then
-	        echo "Insira o novo nome"
-	        read new
-	        sed -i "s/$r_disc/$new/" $f_disc
-					echo $r_disc"->"$new
-	                #Se a disciplina estiver designado a algum aluno altera-se?
-					#Chamada do ficheiro novamente para aparecerem as opções do menu
-					bash "registo.sh"
-	        echo " "
-					break;
+								else
+									echo "ERRO: Este docente não se encontra registado! "
+							fi;;
 
-				else
-	        echo "ERRO: Esta disciplina não se encontra registada! "
-			fi
+					"Disciplina")
+						echo "Aletrar $opt_r"
+						echo " "
+						echo "Insira o nome da Disciplina a alterar:"
+						cat $f_disc
+						read r_disc
+
+						if grep -Fxq $r_disc $f_disc
+							then
+								echo "Insira o novo nome"
+								read new
+								sed -i "s/$r_disc/$new/" $f_disc
+								echo $r_disc"->"$new
+												#Se a disciplina estiver designado a algum aluno altera-se?
+								#Chamada do ficheiro novamente para aparecerem as opções do menu
+								bash "registo.sh"
+								echo " "
+								break;
+
+							else
+								echo "ERRO: Esta disciplina não se encontra registada! "
+						fi
+					;;
+
+					"Estudante")
+
+					;;
+
+					"Menu anterior")
+							bash "main.sh"
+					 ;;
+
+						 *) echo "invalid option $REPLY";;
+				 esac
+			 done
 		;;
 
-		"Estudante")
+		"Alterar")
 
-		;;
+			PS3='Introduza a opção:'
+			options_registo=("Universidade" "Professor" "Disciplina" "Estudante"  "Menu anterior")
 
-		"Menu anterior")
-				bash "main.sh"
-		 ;;
+			select opt_r in "${options_registo[@]}"
+			do
+				case $opt_r in
+					"Universidade")
+						echo "Aletrar $opt_r"
+						echo " "
+						echo "Insira o nome da Universidade a alterar:"
+			      cat $f_uni
+						read r_uni
 
-	     *) echo "invalid option $REPLY";;
-	 esac
- done
+						if grep -Fxq $r_uni $f_uni
+							then
+				        echo "Insira o novo nome"
+				        read new
+				        sed -i "s/$r_uni/$new/" $f_uni
+								sed -i -e "s/$r_uni/$new/g" $f_dados
+								echo $r_uni"->"$new
+								#Chamada do ficheiro novamente para aparecerem as opções do menu
+								bash "registo.sh"
+				        echo " "
+								break;
+
+							else
+				        echo "ERRO: Esta Universidade não se encontra registada! "
+						fi;;
+
+					 "Professor")
+							echo "Aletrar $opt_r"
+							echo " "
+							echo "Insira o nome do Professor a alterar:"
+				      cat $f_profs
+							read r_prof
+
+							if grep -Fxq $r_prof $f_profs
+								then
+			            echo "Insira o novo nome"
+			            read new
+			            sed -i "s/$r_prof/$new/" $f_profs
+									sed -i -e "s/$r_prof/$new/g" $f_dados
+									echo $r_prof"->"$new
+					                #Se o professor estiver designado a algum aluno altera-se?
+									#Chamada do ficheiro novamente para aparecerem as opções do menu
+									bash "registo.sh"
+					        echo " "
+									break;
+
+								else
+					      	echo "ERRO: Este docente não se encontra registado! "
+							fi;;
+
+					"Disciplina")
+						echo "Aletrar $opt_r"
+						echo " "
+						echo "Insira o nome da Disciplina a alterar:"
+			      cat $f_disc
+						read r_disc
+
+						if grep -Fxq $r_disc $f_disc
+							then
+				        echo "Insira o novo nome"
+				        read new
+				        sed -i "s/$r_disc/$new/" $f_disc
+								echo $r_disc"->"$new
+				                #Se a disciplina estiver designado a algum aluno altera-se?
+								#Chamada do ficheiro novamente para aparecerem as opções do menu
+								bash "registo.sh"
+				        echo " "
+								break;
+
+							else
+				        echo "ERRO: Esta disciplina não se encontra registada! "
+						fi
+					;;
+
+					"Estudante")
+
+					;;
+
+					"Menu anterior")
+							bash "main.sh"
+					 ;;
+
+				     *) echo "invalid option $REPLY";;
+				 esac
+			 done
+
+			;;
+		esac
+	done
